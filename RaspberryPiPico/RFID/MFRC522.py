393e9b95924ec2db356e7705b733b33509baf843
rontho14 <70487108+rontho14@users.noreply.github.com>
2025-05-21 18:18:31 -0300

    fixing RFID blocking other features from running

--- RaspberryPiPico/RFID/MFRC522.py
+++ RaspberryPiPico/RFID/MFRC522.py
@@ -1,230 +1,242 @@
 from machine import Pin, SPI
-from os import uname
-
+import utime
 
 class MFRC522:
 
-	OK = 0
-	NOTAGERR = 1
-	ERR = 2
+    OK = 0
+    NOTAGERR = 1
+    ERR = 2
 
-	REQIDL = 0x26
-	REQALL = 0x52
-	AUTHENT1A = 0x60
-	AUTHENT1B = 0x61
+    REQIDL = 0x26
+    REQALL = 0x52
+    AUTHENT1A = 0x60
+    AUTHENT1B = 0x61
 
-	def __init__(self, sck, mosi, miso, rst, cs):
+    def __init__(self, sck, mosi, miso, rst, sda):
+        self.sck = Pin(sck)
+        self.mosi = Pin(mosi)
+        self.miso = Pin(miso)
+        self.rst = Pin(rst, Pin.OUT)
+        self.sda = Pin(sda, Pin.OUT)
 
-		self.sck = Pin(sck, Pin.OUT)
-		self.mosi = Pin(mosi, Pin.OUT)
-		self.miso = Pin(miso)
-		self.rst = Pin(rst, Pin.OUT)
-		self.cs = Pin(cs, Pin.OUT)
+        self.rst.value(0)
+        self.sda.value(1)
 
-		self.rst.value(0)
-		self.cs.value(1)
-		
-		board = uname()[0]
+        # Usa SPI(0) com pinos padrão: SCK=GP18, MOSI=GP19, MISO=GP16
+        self.spi = SPI(0, baudrate=1000000, polarity=0, phase=0)
 
-		if board == 'WiPy' or board == 'LoPy' or board == 'FiPy':
-			self.spi = SPI(0)
-			self.spi.init(SPI.MASTER, baudrate=1000000, pins=(self.sck, self.mosi, self.miso))
-		elif board == 'esp8266':
-			self.spi = SPI(baudrate=100000, polarity=0, phase=0, sck=self.sck, mosi=self.mosi, miso=self.miso)
-			self.spi.init()
-		else:
-			raise RuntimeError("Unsupported platform")
+        self.rst.value(1)
+        self.init()
 
-		self.rst.value(1)
-		self.init()
+    def _wreg(self, reg, val):
+        self.sda.value(0)
+        self.spi.write(bytes([0xff & ((reg << 1) & 0x7e)]))
+        self.spi.write(bytes([0xff & val]))
+        self.sda.value(1)
 
-	def _wreg(self, reg, val):
+    def _rreg(self, reg):
+        self.sda.value(0)
+        self.spi.write(bytes([0xff & (((reg << 1) & 0x7e) | 0x80)]))
+        val = self.spi.read(1)
+        self.sda.value(1)
+        return val[0]
 
-		self.cs.value(0)
-		self.spi.write(b'%c' % int(0xff & ((reg << 1) & 0x7e)))
-		self.spi.write(b'%c' % int(0xff & val))
-		self.cs.value(1)
+    def _sflags(self, reg, mask):
+        self._wreg(reg, self._rreg(reg) | mask)
 
-	def _rreg(self, reg):
+    def _cflags(self, reg, mask):
+        self._wreg(reg, self._rreg(reg) & (~mask))
 
-		self.cs.value(0)
-		self.spi.write(b'%c' % int(0xff & (((reg << 1) & 0x7e) | 0x80)))
-		val = self.spi.read(1)
-		self.cs.value(1)
+    def _tocard(self, cmd, send):
+        recv = []
+        bits = irq_en = wait_irq = n = 0
+        stat = self.ERR
 
-		return val[0]
+        if cmd == 0x0E:
+            irq_en = 0x12
+            wait_irq = 0x10
+        elif cmd == 0x0C:
+            irq_en = 0x77
+            wait_irq = 0x30
 
-	def _sflags(self, reg, mask):
-		self._wreg(reg, self._rreg(reg) | mask)
+        self._wreg(0x02, irq_en | 0x80)
+        self._cflags(0x04, 0x80)
+        self._sflags(0x0A, 0x80)
+        self._wreg(0x01, 0x00)
 
-	def _cflags(self, reg, mask):
-		self._wreg(reg, self._rreg(reg) & (~mask))
+        for c in send:
+            self._wreg(0x09, c)
+        self._wreg(0x01, cmd)
 
-	def _tocard(self, cmd, send):
+        if cmd == 0x0C:
+            self._sflags(0x0D, 0x80)
 
-		recv = []
-		bits = irq_en = wait_irq = n = 0
-		stat = self.ERR
+        i = 2000
+        while True:
+            n = self._rreg(0x04)
+            i -= 1
+            if ~((i != 0) and ~(n & 0x01) and ~(n & wait_irq)):
+                break
 
-		if cmd == 0x0E:
-			irq_en = 0x12
-			wait_irq = 0x10
-		elif cmd == 0x0C:
-			irq_en = 0x77
-			wait_irq = 0x30
+        self._cflags(0x0D, 0x80)
 
-		self._wreg(0x02, irq_en | 0x80)
-		self._cflags(0x04, 0x80)
-		self._sflags(0x0A, 0x80)
-		self._wreg(0x01, 0x00)
+        if i:
+            if (self._rreg(0x06) & 0x1B) == 0x00:
+                stat = self.OK
 
-		for c in send:
-			self._wreg(0x09, c)
-		self._wreg(0x01, cmd)
+                if n & irq_en & 0x01:
+                    stat = self.NOTAGERR
+                elif cmd == 0x0C:
+                    n = self._rreg(0x0A)
+                    lbits = self._rreg(0x0C) & 0x07
+                    if lbits != 0:
+                        bits = (n - 1) * 8 + lbits
+                    else:
+                        bits = n * 8
 
-		if cmd == 0x0C:
-			self._sflags(0x0D, 0x80)
+                    if n == 0:
+                        n = 1
+                    elif n > 16:
+                        n = 16
 
-		i = 2000
-		while True:
-			n = self._rreg(0x04)
-			i -= 1
-			if ~((i != 0) and ~(n & 0x01) and ~(n & wait_irq)):
-				break
+                    for _ in range(n):
+                        recv.append(self._rreg(0x09))
+            else:
+                stat = self.ERR
 
-		self._cflags(0x0D, 0x80)
+        return stat, recv, bits
 
-		if i:
-			if (self._rreg(0x06) & 0x1B) == 0x00:
-				stat = self.OK
+    def _crc(self, data):
+        self._cflags(0x05, 0x04)
+        self._sflags(0x0A, 0x80)
 
-				if n & irq_en & 0x01:
-					stat = self.NOTAGERR
-				elif cmd == 0x0C:
-					n = self._rreg(0x0A)
-					lbits = self._rreg(0x0C) & 0x07
-					if lbits != 0:
-						bits = (n - 1) * 8 + lbits
-					else:
-						bits = n * 8
+        for c in data:
+            self._wreg(0x09, c)
 
-					if n == 0:
-						n = 1
-					elif n > 16:
-						n = 16
+        self._wreg(0x01, 0x03)
 
-					for _ in range(n):
-						recv.append(self._rreg(0x09))
-			else:
-				stat = self.ERR
+        i = 0xFF
+        while True:
+            n = self._rreg(0x05)
+            i -= 1
+            if not ((i != 0) and not (n & 0x04)):
+                break
 
-		return stat, recv, bits
+        return [self._rreg(0x22), self._rreg(0x21)]
 
-	def _crc(self, data):
+    def init(self):
+        self.reset()
+        self._wreg(0x2A, 0x8D)
+        self._wreg(0x2B, 0x3E)
+        self._wreg(0x2D, 30)
+        self._wreg(0x2C, 0)
+        self._wreg(0x15, 0x40)
+        self._wreg(0x11, 0x3D)
+        self.antenna_on()
 
-		self._cflags(0x05, 0x04)
-		self._sflags(0x0A, 0x80)
+    def reset(self):
+        self._wreg(0x01, 0x0F)
 
-		for c in data:
-			self._wreg(0x09, c)
+    def antenna_on(self, on=True):
+        if on and ~(self._rreg(0x14) & 0x03):
+            self._sflags(0x14, 0x03)
+        else:
+            self._cflags(0x14, 0x03)
 
-		self._wreg(0x01, 0x03)
+    def request(self, mode):
+        self._wreg(0x0D, 0x07)
+        (stat, recv, bits) = self._tocard(0x0C, [mode])
 
-		i = 0xFF
-		while True:
-			n = self._rreg(0x05)
-			i -= 1
-			if not ((i != 0) and not (n & 0x04)):
-				break
+        if (stat != self.OK) | (bits != 0x10):
+            stat = self.ERR
 
-		return [self._rreg(0x22), self._rreg(0x21)]
+        return stat, bits
 
-	def init(self):
+    def anticoll(self):
+        ser_chk = 0
+        ser = [0x93, 0x20]
 
-		self.reset()
-		self._wreg(0x2A, 0x8D)
-		self._wreg(0x2B, 0x3E)
-		self._wreg(0x2D, 30)
-		self._wreg(0x2C, 0)
-		self._wreg(0x15, 0x40)
-		self._wreg(0x11, 0x3D)
-		self.antenna_on()
+        self._wreg(0x0D, 0x00)
+        (stat, recv, bits) = self._tocard(0x0C, ser)
 
-	def reset(self):
-		self._wreg(0x01, 0x0F)
+        if stat == self.OK:
+            if len(recv) == 5:
+                for i in range(4):
+                    ser_chk = ser_chk ^ recv[i]
+                if ser_chk != recv[4]:
+                    stat = self.ERR
+            else:
+                stat = self.ERR
 
-	def antenna_on(self, on=True):
+        return stat, recv
 
-		if on and ~(self._rreg(0x14) & 0x03):
-			self._sflags(0x14, 0x03)
-		else:
-			self._cflags(0x14, 0x03)
+    def select_tag(self, ser):
+        buf = [0x93, 0x70] + ser[:5]
+        buf += self._crc(buf)
+        (stat, recv, bits) = self._tocard(0x0C, buf)
+        return self.OK if (stat == self.OK) and (bits == 0x18) else self.ERR
 
-	def request(self, mode):
+    def auth(self, mode, addr, sect, ser):
+        return self._tocard(0x0E, [mode, addr] + sect + ser[:4])[0]
 
-		self._wreg(0x0D, 0x07)
-		(stat, recv, bits) = self._tocard(0x0C, [mode])
+    def stop_crypto1(self):
+        self._cflags(0x08, 0x08)
 
-		if (stat != self.OK) | (bits != 0x10):
-			stat = self.ERR
+    def read(self, addr):
+        data = [0x30, addr]
+        data += self._crc(data)
+        (stat, recv, _) = self._tocard(0x0C, data)
+        return recv if stat == self.OK else None
 
-		return stat, bits
+    def write(self, addr, data):
+        buf = [0xA0, addr]
+        buf += self._crc(buf)
+        (stat, recv, bits) = self._tocard(0x0C, buf)
 
-	def anticoll(self):
+        if not (stat == self.OK) or not (bits == 4) or not ((recv[0] & 0x0F) == 0x0A):
+            stat = self.ERR
+        else:
+            buf = []
+            for i in range(16):
+                buf.append(data[i])
+            buf += self._crc(buf)
+            (stat, recv, bits) = self._tocard(0x0C, buf)
+            if not (stat == self.OK) or not (bits == 4) or not ((recv[0] & 0x0F) == 0x0A):
+                stat = self.ERR
 
-		ser_chk = 0
-		ser = [0x93, 0x20]
+        return stat
 
-		self._wreg(0x0D, 0x00)
-		(stat, recv, bits) = self._tocard(0x0C, ser)
+# ============================
+# Código principal de leitura:
+# ============================
 
-		if stat == self.OK:
-			if len(recv) == 5:
-				for i in range(4):
-					ser_chk = ser_chk ^ recv[i]
-				if ser_chk != recv[4]:
-					stat = self.ERR
-			else:
-				stat = self.ERR
+def read_card(reader):
+    (status, tag_type) = reader.request(reader.REQIDL)
+    if status == reader.OK:
+        print("Cartão detectado!")
+        (status, uid) = reader.anticoll()
+        if status == reader.OK:
+            print("UID do cartão: {}".format(uid))
+            return uid
+    return None
 
-		return stat, recv
+def main():
+    # Configure os pinos conforme sua ligação física!
+    sck = 18
+    mosi = 19
+    miso = 16
+    rst = 15  # Exemplo: GP20
+    sda = 14  # Exemplo: GP17
 
-	def select_tag(self, ser):
+    reader = MFRC522(sck, mosi, miso, rst, sda)
+    print("Aproxime um cartão RFID...")
 
-		buf = [0x93, 0x70] + ser[:5]
-		buf += self._crc(buf)
-		(stat, recv, bits) = self._tocard(0x0C, buf)
-		return self.OK if (stat == self.OK) and (bits == 0x18) else self.ERR
+    while True:
+        uid = read_card(reader)
+        if uid:
+            print("UID lido: {}".format(uid))
+        utime.sleep(0.5)
 
-	def auth(self, mode, addr, sect, ser):
-		return self._tocard(0x0E, [mode, addr] + sect + ser[:4])[0]
-
-	def stop_crypto1(self):
-		self._cflags(0x08, 0x08)
-
-	def read(self, addr):
-
-		data = [0x30, addr]
-		data += self._crc(data)
-		(stat, recv, _) = self._tocard(0x0C, data)
-		return recv if stat == self.OK else None
-
-	def write(self, addr, data):
-
-		buf = [0xA0, addr]
-		buf += self._crc(buf)
-		(stat, recv, bits) = self._tocard(0x0C, buf)
-
-		if not (stat == self.OK) or not (bits == 4) or not ((recv[0] & 0x0F) == 0x0A):
-			stat = self.ERR
-		else:
-			buf = []
-			for i in range(16):
-				buf.append(data[i])
-			buf += self._crc(buf)
-			(stat, recv, bits) = self._tocard(0x0C, buf)
-			if not (stat == self.OK) or not (bits == 4) or not ((recv[0] & 0x0F) == 0x0A):
-				stat = self.ERR
-
-		return stat
+# Executa o programa
+# main()
 
--- RaspberryPiPico/testeIdk.py
+++ RaspberryPiPico/testeIdk.py
@@ -16,15 +16,16 @@ display = SSD1306_I2C(128, 32, i2c)
 led = Pin(15, Pin.OUT)
 buzzer = Pin(28, Pin.OUT)
 
-# Initialize RFID reader with correct pins (match your wiring)
-reader = MFRC522(18, 19, 16, 15, 14)  # SCK=18, MOSI=19, MISO=16, RST=15, SDA=14
+# Initialize RFID reader with correct pins
+reader = MFRC522(18, 19, 16, 15, 14)
 
-# Estado do sistema
 password_entering = False
 entered_password = ""
 correct_password = "12345"
 last_activity_time = utime.time()
 inactive_timeout = 10  # segundos
+rfid_enabled = True  # flag para ativar/desativar RFID
+access_granted_time = None
 
 def beep(times, duration_on=0.05, duration_off=0.05):
     for _ in range(times):
@@ -50,80 +51,42 @@ def check_rfid():
     return None
 
 print("Aproxime um cartão RFID ou digite senha")
-display_message("Aproxime um cartão RFID ou digite senha")
 
-access_granted_time = None
-access_granted = False
-rfid_enabled = True
-display_cleared = False  # True quando display está limpo (sem mensagem)
 
 while True:
     keys = keyboard.get_pressed_keys()
     current_time = utime.time()
 
-    # Se acesso liberado, verifica timeout para limpar display
-    if access_granted:
-        if current_time - access_granted_time >= 10:
-            display.fill(0)    # Limpa o display
-            display.show()
-            access_granted = False
-            rfid_enabled = True  # reativa RFID após limpar display
-            display_cleared = True
-            led.value(0)
-
-    # Timeout para limpar tela e apagar LED após inatividade (quando não estiver com acesso liberado)
-    if (not access_granted) and (current_time - last_activity_time > inactive_timeout):
-        if not display_cleared:
-            display.fill(0)  # Limpa o display
-            display.show()
-            led.value(0)
-            display_cleared = True
-
-    # Se o display está limpo e houver interação, mostra a mensagem inicial
-    if display_cleared:
-        if keys or (rfid_enabled and check_rfid()):
-            display_message("Aproxime um cartão RFID ou digite senha")
-            display_cleared = False
-            last_activity_time = current_time
-
-    # Processa RFID (só se não estiver no modo senha e se RFID estiver habilitado)
-    if not password_entering and rfid_enabled and not display_cleared:
-        uid = check_rfid()
-        if uid:
-            display_message("Acesso Liberado")
-            print(f"UID: {uid}")
-            beep(3, 0.1, 0.1)
-            led.value(1)
-            access_granted_time = current_time
-            access_granted = True
-            rfid_enabled = False  # evita múltiplas leituras do mesmo cartão
-            last_activity_time = current_time
-
-    # Processa teclado
+    # Se houver teclas pressionadas
     if keys:
         for key in keys:
             print(f"Tecla pressionada: {key}")
-            last_activity_time = current_time
 
             if key == "*":
+                # Inicia entrada de senha e desabilita RFID
                 password_entering = True
                 entered_password = ""
                 display_message("Digite a senha")
-                rfid_enabled = False  # desativa RFID ao entrar senha
-                display_cleared = False
+                last_activity_time = current_time
+                rfid_enabled = False
 
             elif password_entering and key in "1234567890":
+                # Construindo senha
                 entered_password += key
                 display_message("Senha: " + "*" * len(entered_password))
+                last_activity_time = current_time
 
             elif key == "#" and password_entering:
+                # Finaliza senha e verifica
                 password_entering = False
                 display_message("Verificando...")
+                last_activity_time = current_time
 
                 if entered_password == correct_password:
                     display_message("Senha OK! Apresente RFID")
                     beep(3, 0.05, 0.05)
-                    # Espera o cartão ser apresentado durante 5 segundos
+
+                    # Espera RFID por 5 segundos
                     start_wait = utime.ticks_ms()
                     uid = None
                     while utime.ticks_diff(utime.ticks_ms(), start_wait) < 5000:
@@ -137,18 +100,44 @@ while True:
                         print(f"UID: {uid}")
                         beep(3, 0.1, 0.1)
                         led.value(1)
-                        access_granted_time = utime.time()
-                        access_granted = True
+                        access_granted_time = utime.time()  # Guarda o tempo do acesso liberado
                     else:
                         display_message("RFID nao detectado")
                         beep(2, 0.3, 0.3)
-
                 else:
                     display_message("Senha incorreta")
                     beep(2, 0.3, 0.3)
 
                 entered_password = ""
-                rfid_enabled = True  # reativa RFID depois da senha
+                last_activity_time = utime.time()
+                rfid_enabled = True  # Reativa RFID após senha
+                
+
+    # Se RFID está habilitado, tenta detectar cartão
+    if rfid_enabled:
+        uid = check_rfid()
+        if uid:
+            display_message("Acesso Liberado")
+            print(f"UID: {uid}")
+            beep(3, 0.1, 0.1)
+            led.value(1)
+            access_granted_time = utime.time()
+            rfid_enabled = False  # Desabilita RFID para não repetir leitura
+
+    # Após liberar acesso, espera 10 segundos para limpar display
+    if access_granted_time is not None:
+        if current_time - access_granted_time > 10:
+            display.fill(0)
+            display.show()
+            access_granted_time = None
+            led.value(0)
+            rfid_enabled = True  # Volta a habilitar RFID para novas leituras
+
+    # Timeout para limpar tela e apagar LED após inatividade
+    if current_time - last_activity_time > inactive_timeout:
+        display.fill(0)
+        display.show()
+        led.value(0)
 
     utime.sleep_ms(100)
 
